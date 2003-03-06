ebbfa7b8ea3255657f9a52e3f7b8a7a857c72ebf
Tom Schindl <tom.schindl@bestsolution.at>
2003-03-06 13:24:20 +0000

    Import of TOMSON/Apache-AxKit-Language-Svg2AnyFormat-0.02 from CPAN.
    gitpan-cpan-distribution: Apache-AxKit-Language-Svg2AnyFormat
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         TOMSON/Apache-AxKit-Language-Svg2AnyFormat-0.02.tar.gz
    gitpan-cpan-author:       TOMSON
    gitpan-cpan-maturity:     released

--- lib/Apache/AxKit/Language/Svg2AnyFormat.pm
+++ lib/Apache/AxKit/Language/Svg2AnyFormat.pm
@@ -3,11 +3,11 @@ package Apache::AxKit::Language::Svg2AnyFormat;
 @ISA = ( 'Apache::AxKit::Language' );
 
 BEGIN {
-   $Apache::AxKit::Language::Svg2AnyFormat::VERSION = 0.01
+   $Apache::AxKit::Language::Svg2AnyFormat::VERSION = 0.02
 }
 
 use Apache;
-use Image::Magick;
+# use Image::Magick;
 use Apache::Request;
 Apache::AxKit::Cache;
 use File::Copy ();
@@ -22,15 +22,26 @@ my $cache;
 
 my %Config = 
 (  
-    SVGOutputMimeType => "image/png"
+    SVGOutputMimeType   => "image/png",
+    SVGOutputSerializer => "ImageMagick",
+    SVGOutputLibRSVGBin => "/usr/local/bin"
 );
 
 my %MimeTypeSuffixMappings =
 (
-    "image/png"  => "png",
-    "image/jpeg" => "jpg",
-    "image/gif"  => "gif",
-    "application/pdf"  => "pdf"
+    "image/png"              => "png",
+    "image/jpeg"             => "jpg",
+    "image/gif"              => "gif",
+    "application/pdf"        => "pdf",
+    "application/postscript" => "eps"
+);
+
+my %MimeTypeLibRSVGFormatMappings =
+(
+    "image/png"        => "png",
+## DOES NOT WORK CORRECTLY AT THE MOMENT
+## ONE CAN TURN ON BY COMMENTING IN
+##    "image/jpeg"       => "jpeg"
 );
 
 sub stylesheet_exists () { 0; }
@@ -42,8 +53,12 @@ sub handler
     
     my $mime;
     my $suffix = "png";
+    my $serializer;
+    my $rsvg_bin;
     
-    print STDERR "CAME IN TO IT\n";
+    AxKit::Debug(8, "Transform started!!!!");
+    
+    print STDERR "IN HERE!!!!!!\n";
     
     if( ! $last_in_chain )
     {
@@ -52,27 +67,54 @@ sub handler
     
     if( $r->pnotes( "axkit_mime_type" ) )
     {
-        $mime = $r->pnotes( "axkit_mime_type" )
+        AxKit::Debug(8, "MimeType retrieved from Plugin");
+        $mime = $r->pnotes( "axkit_mime_type" );
     }
     else
     {
+        AxKit::Debug(8, "MimeType retrieved from CONF or using Default");
         $mime = $r->dir_config( "SVGOutputMimeType" ) || $Config{SVGOutputMimeType};
     }
     
+    AxKit::Debug(8, "MimeType is set to '$mime'");
     
     if( ! exists $MimeTypeSuffixMappings{$mime} )
     {
+        AxKit::Debug(8, "MimeType is not known. We are using DEFAULTS");
         $mime   = $Config{SVGOutputMimeType};
         $suffix = "png";
     }
     else
     {
+        AxKit::Debug(8, "Setting suffix. To mapped value");
         $suffix = $MimeTypeSuffixMappings{$mime};
+    }
+
+    $serializer = $r->dir_config( "SVGOutputSerializer" ) || $Config{SVGOutputSerializer};
+    $rsvg_bin   = $r->dir_config( "SVGOutputLibRSVGBin" ) || $Config{SVGOutputLibRSVGBin};
+    
+    if( $serializer eq "ImageMagick" || ! exists $MimeTypeLibRSVGFormatMappings{$mime} ) 
+    {
+        AxKit::Debug(8, "We need Image-Magick because ImageMagick should be used as serializer or LibRSVG could not create desired format");
+        
+        ## Loading at runtime
+        require Image::Magick;
+    }
+    elsif( $serializer eq "LibRSVG" )
+    {
+        AxKit::Debug(8, "LibRSVG is registered as serializer");
+        
+        ## nothing to be loaded because we are using command line
+    }
+    else
+    {
+        fail( "This is an unknown serializer for me." );
     }
     
     my $tempdir = File::Temp::tempdir();
-    my $image = new Image::Magick();
-
+    
+    AxKit::Debug(8, "Got tempdir: $tempdir");
+    
     if ( ! $tempdir ) 
     {
         die "Cannot create tempdir: $!";
@@ -82,6 +124,7 @@ sub handler
     
     if( my $dom = $r->pnotes('dom_tree') )
     {
+        AxKit::Debug(8, "Got a dom tree");
         my $xmlstring = $dom->toString();
         delete $r->pnotes()->{'dom_tree'};
         
@@ -93,6 +136,7 @@ sub handler
     }
     elsif( my $xmlstring = $r->pnotes('xml_string') )
     {
+        AxKit::Debug(8, "Got a xml-string");
         my $fh = Apache->gensym();
         chdir( $tempdir ) || fail( "Cannot cd: $!" );
         open($fh, ">temp.svg") || fail( "Cannot write: $!" );
@@ -105,12 +149,15 @@ sub handler
         
         if ( $@ ) 
         {
+            AxKit::Debug(8, "No ref");
             my $fh = $xml_provider->get_fh();
             chdir($tempdir) || fail("Cannot cd: $!");
             File::Copy::copy($fh, "temp.svg");
         }
         else 
         {
+            AxKit::Debug(8, "It has been a ref");
+            
             my $fh = Apache->gensym();
             chdir($tempdir) || fail( "Cannot cd: $!" );
             open($fh, ">temp.svg") || fail( "Cannot write: $!" );
@@ -121,15 +168,81 @@ sub handler
     
     chdir( $tempdir ) || fail("Cannot cd: $!");
     
-    my $retval = $image->Read( "temp.svg" );
-    $image->Write( "temp.$suffix" );
+    my $retval;
     
+    if( $serializer eq "ImageMagick" )
+    {
+        AxKit::Debug(8, "Serializer is ImageMagick");
+        
+        my $image = new Image::Magick();
+        $retval = $image->Read( "temp.svg" );
+        
+        if( "$retval" )
+        {
+            fail( "ImageMagick failed. Reason: $retval" );
+        }
+        
+        $image->Write( "temp.$suffix" );
+    }
+    else
+    {
+        AxKit::Debug(8, "Serializer is: LibRSVG");
+        
+        if( exists $MimeTypeLibRSVGFormatMappings{$mime} )
+        {
+            AxKit::Debug(8, "MimeType is supported by LibRSVG");
+            
+            $retval = system( "$rsvg_bin/rsvg -f ".$MimeTypeLibRSVGFormatMappings{$mime}." temp.svg temp.$suffix" );
+    
+
+            if( $retval )
+            {
+                fail( "rsvg exited with status code $retval" );
+            }
+        }
+        else
+        {
+            AxKit::Debug(8, "MimeType '$mime' **NOT** is supported by LibRSVG");
+            
+            my $image = new Image::Magick();
+
+            AxKit::Debug(8, "STEP 1: rsvg convert to PNG");
+            
+            $retval = system( "$rsvg_bin/rsvg -f png temp.svg temp.png" );
+            
+            if( $retval )
+            {
+                fail( "rsvg exited with status code $retval" );
+            }
+            
+            AxKit::Debug(8, "STEP 2: ImageMagick to FINAL format '$mime'");
+
+            chdir( $tempdir ) || fail("Cannot cd: $!");
+
+            $retval = $image->Read( "temp.png" );
+            
+            if( "$retval" )
+            {
+                fail( "ImageMagick failed. Reason: $retval" );
+            }
+            
+            $image->Write( "temp.$suffix" );
+            
+            if( "$retval" )
+            {
+                fail( "ImageMagick failed. Reason: $retval" );
+            }
+        }
+    }
+
+    AxKit::Debug(8, "Serialization finished.");
+
     $AxKit::Cfg->AllowOutputCharset(0);
     
     my $pdfh = Apache->gensym();
     
     open( $pdfh, "<temp.$suffix" ) or fail( "Could not open $mime: $!" );
-    $r->content_type($mime);
+    $r->content_type( $mime );
     local $/;
     
     $r->print(<$pdfh>);
@@ -159,6 +272,8 @@ __END__
 Apache::AxKit::Language::Svg2AnyFormat - SVG Serializer
 
 =head1 SYNOPSIS
+
+=head2 ImageMagick
 
   AddHandler axkit .svg
 
@@ -191,18 +306,140 @@ Apache::AxKit::Language::Svg2AnyFormat - SVG Serializer
     AxAddPlugin Apache::AxKit::Plugin::SvgCgiSerialize   
   </Files>
 
+=head2 LibRSVG
+
+  AddHandler axkit .svg
+
+  ## Fairly important to cache the output because
+  ## transformation is highly CPU-Time and Memory consuming
+  AxCacheDir /tmp/axkit_cache
+
+  ## When using SvgCgiSerialize this is vital 
+  ## because the cgi-parameters are not used
+  ## by default to build the cache
+  AxAddPlugin Apache::AxKit::Plugin::QueryStringCache
+
+  <Files ~ *.svg>
+    AxAddStyleMap application/svg2anyformat Apache::AxKit::Language::Svg2AnyFormat
+    AxAddProcessor application/svg2anyformat NULL
+
+    ## optional with this variable you can
+    ## overwrite the default output format 
+    ## PNG
+    ## Supported Values(Native Formats):
+    ##    image/png
+    ## If you specify any other format:
+    ##   svg->png is done by LibRSVG
+    ##   png->chosen format Image::Magick
+    PerlSetVar SVGOutputMimeType image/jpeg
+    
+    PerlSetVar SVGOutputSerializer LibRSVG
+    
+    ## only to be set if path differs from
+    ## /usr/local/bin
+    PerlSetVar SVGOutputLibRSVGBin /usr/bin/rsvg
+    
+    ## optional module to pass the format using cgi-parameters
+    ## to the module. For supported values see above
+    ## and the man-page of the plugin
+    AxAddPlugin Apache::AxKit::Plugin::SvgCgiSerialize   
+  </Files>
+
+
 =head1 DESCRIPTION
 
 Svg2AnyFormat is a serializer which can transform SVG to many different
-output formats(e.g. png, jpg, ... ). At the moment it uses Image::Magick as conversion library
-which does not support the whole set of svg features. In one case the conversion
+output formats(e.g. png, jpg, ... ). At the moment it uses Image::Magick or LibRSVG as conversion libraries
+which do not support the whole set of svg features. In one case the conversion
 could work in another not. You have to give it a try. Please note because 
 Svg2AnyFormat to any format is a searializer it HAS TO BE LAST in the transformer 
 chain!!!!
 
+Please note when referencing external material (e.g. Images) you'll have to use an absolute path
+
+=head2 Image::Magick
+
+If no SVGOutputSerializer is set Image::Magick is used as default. The reason is simply
+because of backward compatility. You could also set Image::Magick explicitly with
+
+=head3 Example:
+
+  PerlSetVar SVGOutputSerializer ImageMagick
+
+=head3 Advantges:
+
+=over
+
+=item 
+
+Nearly any format can be exported
+
+=item 
+
+known to work on many os
+
+=back
+
+=head3 Disadvantages:
+
+=over
+
+=item 
+
+it's fairly big
+
+=item 
+
+it does not support as much of the SVG-Spec as LibRSVG
+
+=back
+
+=head2 LibRSVG
+
+LibRSVG is part of the gnome project. And could also be used as SVG-Serializer at the moment
+the only really supported output-format is PNG. As a matter of that if you want to use
+LibRSVG as your SVG-Serializer and the output format is an other than PNG, LibRSVG is used to
+transform the SVG to PNG and ImageMagick from PNG to the desired output format.
+At the moment no working PERL-Module for LibRSVG exists so we are using the commandline utility
+rsvg.
+
+=head3 Example:
+
+  PerlSetVar SVGOutputSerializer LibRSVG
+  PerlSetVar SVGOutputLibRSVGBin /usr/bin/rsvg
+
+=head3 Advantages
+
+=over
+
+=item 
+
+supports more of SVG-spec than Image::Magick
+
+=item
+
+not that big
+
+=back
+
+=head3 Disadvantages:
+
+=over
+
+=item 
+
+no Perl-Module for C-libary => command line used at the moment
+
+=item
+
+only PNG supported as output format. This is solved by using
+Image::Magick in a second transformation step (LOW Performance!!!).
+
+=back
+
 =head1 VERSION
 
-0.01
+0.02
 
 =head1 SEE ALSO
 
